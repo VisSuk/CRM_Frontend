3eb27b1817f4a1b7b6d19682bee55b9e1e576920
unknown <vishnusukesh02@gmail.com>
2026-01-11 19:24:01 +0530

    TaskCard CSS Import Fix

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
-import "./taskcard.css"
+import "./Taskcard.css"
 import TaskMenu from "../users/components/TaskMenu"
 import { getFormattedDate } from "../functions/formattedDate"
 
