87a49a53ddc81ac8ea099bd85d14017185289f16
unknown <vishnusukesh02@gmail.com>
2026-01-27 18:22:59 +0530

    Fix Header for new UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,15 @@
 import React, { useState } from "react"
+import '../css/commonStyles.css'
 import '../css/Header.css'
 import { useNavigate, useLocation } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPowerOff, faSuitcase } from "@fortawesome/free-solid-svg-icons"
 
 
-function Header({onSearch}) {
-  const [showMenu, setShowMenu] = useState(false)
+function Header({ onSearch }) {
   const navigate = useNavigate()
-  const location = useLocation()
 
-  const showSearch =
-    location.pathname.startsWith("/tasks") ||
-    location.pathname.startsWith("/admin-tasks")
+
 
   const handleLogout = () => {
     sessionStorage.clear()
@@ -20,19 +19,13 @@ function Header({onSearch}) {
   return (
     <div className="header">
 
-      <div className="header-right">
-        <img
-          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2017%2F06%2F13%2F12%2F53%2Fprofile-2398782_960_720.png&f=1&nofb=1&ipt=3cc863969707805a51958e60119caddc4ebc57de0a188ab8c2891151a57acaba"
-          alt="profile"
-          className="profile-img"
-          onClick={() => setShowMenu(!showMenu)}
-        />
+      <div className="header-left">
+        <div className="logo box-icon"><FontAwesomeIcon icon={faSuitcase} /></div>
+        <h2>Mini CRM Pro</h2>
+      </div>
 
-        {showMenu && (
-          <div className="profile-menu">
-            <button onClick={handleLogout}>Logout</button>
-          </div>
-        )}
+      <div className="header-right">
+        <FontAwesomeIcon className="power-icon" icon={faPowerOff} onClick={handleLogout}/>
       </div>
 
     </div>
